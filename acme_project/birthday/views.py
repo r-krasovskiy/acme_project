ca798b91f4a9506d6e491df12cfe8c7da8487ce4
Роман Красовский <krasovskiyroman@gmail.com>
2023-11-12 22:47:36 +0300

    database connected to POST web form

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,11 +4,13 @@ from django.shortcuts import render
 from .forms import BirthdayForm
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+# Импортируем модель дней рождения.
+from .models import Birthday
 
 
 def birthday(request):
     # Создаём экземпляр класса формы.
-    form = BirthdayForm(request.GET or None)
+    form = BirthdayForm(request.POST or None)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если в GET-запросе были переданы параметры — значит,
@@ -18,6 +20,8 @@ def birthday(request):
     # — это идентично обычному BirthdayForm()
     # Если форма валидна...
     if form.is_valid():
+        # Сохраняем в базу введенные данные.
+        form.save()
         # ...вызовем функцию подсчёта дней:
         birthday_countdown = calculate_birthday_countdown(
             # ...и передаём в неё дату из словаря cleaned_data.
@@ -27,3 +31,11 @@ def birthday(request):
         context.update({'birthday_countdown': birthday_countdown})
     # Указываем нужный шаблон и передаём в него словарь контекста.
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context) 
